ac77d8e7c6ce33a695f2584b1c1f3dd43febafb9
Mayank Kumar <mayankmtr08@gmail.com>
2026-03-01 10:33:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// console.log("hello world");
+// document.write("HELLO WORLD");
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num = "Sanjeev aiml";
+// console.log(num);
+// console.log(typeof(num));
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+// num = undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+// //function
+// var sayHello = function(){
+//     alert("Hello world");
+// }
+
+// //array
+// var numberArray = [1,2,3];
+// var animals = new Array("dog","cat","tiger");
+
+// //object
+// var person = {
+//     name : "Barack Hussain Obama",
+//     age : 22,
+//     title : "44th president of the united states"
+// }
+
+// const syml = Symbol(4);
+// console.log(syml);
+// const sym2 = Symbol(4);
+// console.log(sym2);
+// if(syml == sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+var a = 100;
+var b = 13;
+var c = 10;
+var linebreak = "<br>";
+document.write("a+b+c = ");
+result = a + b + c;
+document.write(result);
+document.write(linebreak);
