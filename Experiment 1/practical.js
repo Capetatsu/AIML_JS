f66bd94ed97d2d70a1a4e395a573f3ac84d7b7f0
Mayank Kumar <mayankmtr08@gmail.com>
2026-06-17 23:48:11 +0530

    Resolve merge conflict in Experiment 1

--- Experiment 1/practical.js
+++ Experiment 1/practical.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function calc(){
         let sub = document.getElementById("subjects").value;
         let sum = 0;
@@ -33,40 +32,4 @@ function calc(){
         }
 
         document.getElementById("result").innerHTML ="Average Marks: " + avg.toFixed(2) + "<br>Grade: " + grade + "<br>Result: " + result + "<br/>";
-=======
-function calc(){
-        let sub = document.getElementById("subjects").value;
-        let sum = 0;
-        for(let i=0;i<sub;i++){
-            let marks = parseFloat(prompt("Enter marks for subject " + (i+1)));
-            sum = sum + marks;
-        }
-        let avg = sum / sub;
-        let grade;
-        if(avg >= 90){
-            grade = "A";
-        }
-        else if(avg >= 80){
-        grade = "B";
-        }
-        else if(avg >= 70){
-        grade = "C";
-        }
-        else if(avg >= 60){
-        grade = "D";
-        }
-        else{
-        grade = "F";
-        }
-
-        if(avg >= 40){
-            result = "Pass";
-        }
-
-        else{
-            result = "Fail";
-        }
-
-        document.getElementById("result").innerHTML ="Average Marks: " + avg.toFixed(2) + "<br>Grade: " + grade + "<br>Result: " + result + "<br/>";
->>>>>>> d24058b74fffeb1001971b1043a18ead84fac348
     }
